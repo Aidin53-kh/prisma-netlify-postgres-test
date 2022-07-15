4193f2ffc5b9b2bbcd30ac02f1aba76302ad6147
Aidin53 <aidin53.110@gmail.com>
2022-07-15 06:34:31 -0700

    init2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,30 @@
 import React from "react";
-import Link from "next/link";
+import type { GetStaticProps } from "next";
+import { db } from "../../prisma/db";
 
-function Home() {
+export const getStaticProps: GetStaticProps = async () => {
+    const users = await db.user.findMany();
+    console.log(users);
+    return {
+        props: { users },
+    };
+};
+
+function Home({ users }: any) {
+    console.log(users);
     return (
-        <div className="h-screen flex flex-col items-center justify-center">
-            asdfasdf
+        <div>
+            <h2 className="text-xl">All User</h2>
+            <div className="my-8">
+                {users.map((user) => {
+                    return (
+                        <div className="p-4 m-4 border rounded-t-md">
+                            <p>name: {user.username}</p>
+                            <p>email: {user.email}</p>
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
 }
